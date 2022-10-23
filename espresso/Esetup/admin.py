6c9e3c25c91b2723eafb42c69fc535f3e2953383
Chirag Kathpalia <chiragkathpalia1@gmail.com>
2022-10-23 12:55:18 +0530

    p1

---
+++ espresso/Esetup/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from Esetup.models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+# Register your models here.
+class UserModel(UserAdmin):
+    pass 
+admin.site.register(CustomUser, UserModel)
