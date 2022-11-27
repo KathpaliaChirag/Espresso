bc0b76ef6a2016fb74d7e4fe01cde9b714d3ffa5
Chirag Kathpalia <chiragkathpalia1@gmail.com>
2022-11-27 21:20:51 +0530

    unstable

--- espresso/Esetup/hodviews.py
+++ espresso/Esetup/hodviews.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from Esetup.models import Courses, Session
+from Esetup.models import Courses, Session, CustomUser, students
+from django.contrib import messages
 @login_required(login_url='/') #used taki cookie ki wajha se logout baad login na ho as saved info
 def adminhome(request):
     return render(request, "hod/hodbase.html")
@@ -9,6 +10,56 @@ def adminhome(request):
 def addstudent(request):
     course = Courses.object.all()
     session = Session.object.all()
+    if request.method == 'POST':
+        profile_pic = request.FILES.get('profile_pic')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        address = request.POST.get('address')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        gender = request.POST.get('gender')
+        course_selected = request.POST.get('course')
+        session_selected = request.POST.get('session')
+
+        if CustomUser.objects.filter(email= email).exists():
+            messages.warning(request, "email exists for another user")
+            return redirect('addstudent')
+        if CustomUser.objects.filter(username= username).exists():
+            messages.warning(request, "email exists for another user")
+            return redirect('addstudent')
+        else: 
+            user= CustomUser (
+                first_name = first_name,
+                last_name = last_name,
+                email = email,
+                username= username,
+                # password= password,
+                display_pic = profile_pic,
+                user_type = 3
+            )
+
+            user.set_password = password
+            user.save()
+
+            course = Courses.object.get(id = course_selected)
+            session = Session.object.get(id = session_selected)
+           
+            student = students(
+                admin= user,
+                address = address,
+                # session_id = session,
+                course_id = course,
+                gender = gender
+            )
+            student.save()
+            messages.success(request, 'Successfully added student')
+            return redirect("addstudent")
+            
+
+            
+        # print(address, session, password, course)
+    
     context = {
         "course": course,
         'session': session,
